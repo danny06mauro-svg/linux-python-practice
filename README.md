2d71263cc67ef63ff1be32d35fcd686d3ee4a653
Danny Mauro <danny06mauro@gmail.com>
2026-09-17 16:39:53 -0400

    Add README and first Python program

---
+++ README.md
@@ -0,0 +1,4 @@
+# Linux and Python Practice
+
+Hands-on practice with Linux, Python, and Git as I build toward computational nuclear engineering projects.
+
---
+++ hello.py
@@ -0,0 +1,15 @@
+print("hello, world!")
+name = ("danny")
+
+print(name)
+age = 20
+print(name)
+print(age)
+reactorpower = float(input("enter the reactor power "))
+efficiency = 0.33
+electricalpower = (reactorpower) * (efficiency)
+if electricalpower > 3000:
+	print("WARNING")
+print("electrical power:", electricalpower, "MW")
+
+
