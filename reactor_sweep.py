87f620912c87a806a89f6b0f96a5fa734125931a
Danny Mauro <danny06mauro@gmail.com>
2026-09-18 11:32:59 -0400

    compare reactor outputs at diff efficiencies

--- reactor_sweep.py
+++ reactor_sweep.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+def electrical_output(thermal_power, efficiency):
+	return thermal_power * efficiency
 thermal_power = np.linspace(1000, 4000, 7)
 efficiency = 0.33
-electrical_power = thermal_power * efficiency
-
+electrical_power = electrical_output(thermal_power, efficiency)
+electrical_power_40 = electrical_output(thermal_power, 0.40)
 print(electrical_power)
-plt.xlabel("Thermal Power (MW)")
+print(electrical_output(3000, 0.4))
 
+plt.plot(thermal_power, electrical_power, marker="o", label="33% efficiency")
+plt.plot(thermal_power, electrical_power_40, marker="o", label="40% efficiency")
+plt.xlabel("Thermal Power (MW)")
 plt.ylabel("Electrical Power (MW)")
-plt.plot(thermal_power, electrical_power, marker="o")
+plt.legend()
+plt.savefig("reactor_power.png")
